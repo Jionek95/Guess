11260db6f9447a1f98263fb073fa67e519926643
Jionek95 <jakopiczko@wp.pl>
2023-11-08 13:42:32 +0100

    Update Guess class

--- src/main/java/com/jionek/Guess.java
+++ src/main/java/com/jionek/Guess.java
@@ -2,7 +2,7 @@ package com.jionek;
 
 public class Guess {
     public String guess(int randomNum) {
-        return randomNum >=0 ? "You guessed right" : "You didn't guess";
+        return randomNum == getRandomNum() ? "You guessed right" : "You didn't guess";
     }
 
     public int getRandomNum() {
