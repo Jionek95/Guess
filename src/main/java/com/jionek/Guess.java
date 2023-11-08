8a47cc2f4d4f0ce1d77b39af57186e56b3c27e71
Jionek95 <jakopiczko@wp.pl>
2023-11-08 11:44:55 +0100

    Add first @Test method

--- src/main/java/com/jionek/Guess.java
+++ src/main/java/com/jionek/Guess.java
@@ -1,7 +1,11 @@
 package com.jionek;
 
 public class Guess {
-    public String guess() {
+    public String guess(int randomNum) {
         return "You won";
+    }
+
+    public int getRandomNum() {
+        return 0;
     }
 }
--- src/test/java/com/jionek/GuessTest.java
+++ src/test/java/com/jionek/GuessTest.java
@@ -8,7 +8,15 @@ public class GuessTest {
     @Test
     public void simpleWinGuess(){
         Guess game = new Guess();
-        String msg = game.guess();
+        int randomNum = game.getRandomNum();
+        String msg = game.guess(randomNum);
         Assertions.assertEquals("You won", msg);
     }
+
+//    @Test
+//    public void simpleWinGuess(){
+//        Guess game = new Guess();
+//        String msg = game.guess();
+//        Assertions.assertEquals("You won", msg);
+//    }
 }
