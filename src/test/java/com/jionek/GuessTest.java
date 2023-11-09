36ea5b0f9ff2719b971ad7a3dbd9f74ea04984ff
Jionek95 <jakopiczko@wp.pl>
2023-11-09 10:54:32 +0100

    Create 4th test method -> testRandomNumGeneration(): - create random number <1,10> for 100 times - create array[10] - for every array[randomNum] = 1 - sum of arrray should be 10 if getRandom() is accurate

--- src/test/java/com/jionek/GuessTest.java
+++ src/test/java/com/jionek/GuessTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 public class GuessTest {
 
     private Guess game;
@@ -22,15 +24,28 @@ public class GuessTest {
 
     @Test
     public void oneWrongNegativeGuess(){
-        Guess game = new Guess();
         String msg = game.guess(-1);
         Assertions.assertEquals("You didn't guess", msg);
     }
+
     @Test
     public void oneWrongPositiveGuess(){
-        Guess game = new Guess();
         int randomNum = game.getRandomNum();
         String msg = game.guess(randomNum + 1);
         Assertions.assertEquals("You didn't guess", msg);
+    }
+
+    @Test
+    public void testRandomNumGeneration(){
+        // 1 2 3 4 5 6 7 8 9 10
+        // 1 1 1 1 0 0 1 0 1 1
+        int[] rndNumCount = new int[10];
+//        int sum = 0;
+        for (int counter = 0; counter<100; counter++){
+            int randomNum = game.getRandomNum();
+            rndNumCount[randomNum-1] = 1;
+        }
+        int sumOfRndNumCount = Arrays.stream(rndNumCount).sum();
+        Assertions.assertEquals(10, sumOfRndNumCount);
     }
 }
