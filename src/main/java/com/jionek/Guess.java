2dcc3bf7814f4e5a4ccbda2fbcd88a454fb133d9
Jionek95 <jakopiczko@wp.pl>
2023-11-09 10:55:12 +0100

    Implement getRandom() for testRandomNumGeneration():

--- src/main/java/com/jionek/Guess.java
+++ src/main/java/com/jionek/Guess.java
@@ -1,11 +1,22 @@
 package com.jionek;
 
+import java.util.Random;
+
 public class Guess {
     public String guess(int randomNum) {
         return randomNum == getRandomNum() ? "You guessed right" : "You didn't guess";
     }
 
     public int getRandomNum() {
-        return 0;
+        Random random = new Random();
+        int randomNum = random.nextInt(1,11);
+        return randomNum;
     }
+
+//    public static void main(String[] args) {
+//
+//        for (int count = 0; count<20; count++) {
+//            System.out.println("My random number NO. " + count + " is : " + getRandomNum());
+//        }
+//    }
 }
